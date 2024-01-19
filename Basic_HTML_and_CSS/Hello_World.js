3a1dbe152279f990f8462c98ba9859ff087e723d
Kevin Tran-Nguyen <kev.vu.ng@gmail.com>
2024-01-19 13:48:05 -0800

    CSS Demo
    CSS Demo Practice

---
+++ Basic_HTML_and_CSS/Hello_World.js
@@ -0,0 +1,11 @@
+var greeting = 'Hello, world!';
+console.log (greeting);
+
+Var greeting2 = 'Welcome to Node.js'
+console.log(greeting2);
+
+Var sentence1 = 'Learning how to debug code with the debugger'
+console.log(sentence1);
+
+var 
+
